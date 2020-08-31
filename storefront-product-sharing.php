9ef11d36a37f1b918bc16275284dddd6c4d80c7a
Luis Rosales <eng.luisrosales@gmail.com>
2020-08-31 13:42:57 +0200

    Fixed fontawesome being loaded twice when storefront or childs are active

--- storefront-product-sharing.php
+++ storefront-product-sharing.php
@@ -215,12 +215,6 @@ final class Storefront_Product_Sharing {
 	 * @return  void
 	 */
 	public function sps_styles() {
-		global $storefront_version;
-
-		if ( version_compare( $storefront_version, '2.3.0', '>=' ) ) {
-			wp_enqueue_style( 'font-awesome-5-brands', '//use.fontawesome.com/releases/v5.0.13/css/brands.css' );
-		}
-
 		wp_enqueue_style( 'sps-styles', plugins_url( '/assets/css/style.css', __FILE__ ) );
 	}
 
